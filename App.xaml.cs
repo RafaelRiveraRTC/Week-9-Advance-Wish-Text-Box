388f9d33b8e0128ea412e6a029e8e31a3e8deae1
Emiliano <emilianovolk75@gmail.com>
2024-05-29 19:12:47 -0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Week_9_Advance_Wish_Text_Box
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Week_9_Advance_Wish_Text_Box
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+
+
+            // FlowDocument - Full Page - Collection of Paragraphs
+            FlowDocument fd = new FlowDocument();
+
+            rtbDisplay.Document = fd;
+
+            //Paragraph - Colelction of sentences
+
+            Paragraph firstParagraph = new Paragraph();
+
+            //Run- Single Sentence 
+
+            Run singleSentence = new Run("Single Sentences for Super Sentient Serpentines");
+            singleSentence.FontStyle
+            // Paragraph.InLines ios a collection you add a run to
+            firstParagraph.Inlines.Add(singleSentence);
+
+            //Flowdocument.Blocks is a collection you add a paragraph to 
+
+            fd.Blocks.Add(firstParagraph);
+
+        }// MainWindow
+
+
+
+
+    }//class
+}
